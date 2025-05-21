8f20da41ff6c40dda60a4d79d2e4f451da3e8a16
Josia Mirande <josiamiranda3@outlook.com>
2025-05-21 19:39:34 -0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withScrollPositionRestoration } from '@angular/router';
+import { provideRouter} from '@angular/router';
 import { provideHttpClient } from '@angular/common/http'; 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withScrollPositionRestoration('enabled')), provideClientHydration(withEventReplay()),provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),provideHttpClient()]
 };
